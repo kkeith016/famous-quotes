8a8fd4f96a8279817eb27b4268266108c6dd44eb
Kylee <kkeith.biz@gmail.com>
2025-10-06 10:18:23 -0400

    initial push

---
+++ src/main/java/com/pluralsight/FamousQuote.java
@@ -0,0 +1,11 @@
+package com.pluralsight;
+
+import java.util.Scanner;
+
+public class FamousQuote {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+
+    }
+}
